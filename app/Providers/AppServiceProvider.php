bdb5e4d04c720be1b0e9bacbd8aa96a544871297
Tamas Simon <thomas.simon@pushgaming.com>
2018-12-09 11:53:57 +0000

    Remove useless code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(env('REDIRECT_HTTPS')) {
-            $this->app['url']->formatScheme('https');
-        }
     }
 
     /**
