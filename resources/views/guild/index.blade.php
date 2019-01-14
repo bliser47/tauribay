a6db3072be6fa49c7764ea03fdc688930df6762e
Tamas Simon <thomas.simon@pushgaming.com>
2019-01-14 02:03:47 +0000

    Take players to armory for now

--- resources/views/guild/index.blade.php
+++ resources/views/guild/index.blade.php
@@ -47,34 +47,32 @@
             </div>
         </div>
         --}}
-        <div class="col-md-12">
-            <div class="panel panel-default nomargin">
-                <table class="table table-bordered table-classes">
-                    <tr class="tHead">
-                        <th>{{ __("Boss") }}</th>
-                        <th>{{ __("Nehézség") }}</th>
-                        <th>{{ __("Dátum") }}</th>
-                        <th>{{ __("Idő") }}</th>
-                    </tr>
-                    @foreach( $guildEncounters as $encounter )
-                        @if ( array_key_exists($encounter->encounter_id, $encounterIDs))
-                            <tr>
-                                <td class="cellMobile" style="white-space:nowrap;">{{ array_key_exists($encounterIDs[$encounter->encounter_id]["name"],TauriBay\Encounter::ENCOUNTER_NAME_SHORTS) ? TauriBay\Encounter::ENCOUNTER_NAME_SHORTS[$encounterIDs[$encounter->encounter_id]["name"]] : $encounterIDs[$encounter->encounter_id]["name"] }}</td>
-                                <td class="cellDesktop" style="white-space:nowrap;">{{ $encounterIDs[$encounter->encounter_id]["name"] }}</td>
-                                <td class="cellDesktop">{{ TauriBay\Encounter::SIZE_AND_DIFFICULTY[$encounter->difficulty_id] }}</td>
-                                <td class="cellMobile">{{ TauriBay\Encounter::SIZE_AND_DIFFICULTY_SHORT[$encounter->difficulty_id] }}</td>
-                                <td class="cellDesktop">{{ date('M d, Y', $encounter->killtime) }}</td>
-                                <td class="cellMobile">{{ date('M d', $encounter->killtime) }}</td>
-                                <td><a class="guildClearTime" href="{{ URL::to("/encounter/") . "/" . TauriBay\Encounter::getUrlName($encounterIDs[$encounter->encounter_id]["name"]) . "/" . $encounter->id }}">{{ $encounter->fight_time/1000  }}</a></td>
-                            </tr>
-                        @endif
-                    @endforeach
-                </table>
-            </div>
-            <div class="text-center">
-                <div>
-                    {{ $guildEncounters->appends(Illuminate\Support\Facades\Input::except('page')) }}
-                </div>
+        <div class="panel panel-default nomargin">
+            <table class="table table-bordered table-classes">
+                <tr class="tHead">
+                    <th>{{ __("Boss") }}</th>
+                    <th>{{ __("Nehézség") }}</th>
+                    <th>{{ __("Dátum") }}</th>
+                    <th>{{ __("Idő") }}</th>
+                </tr>
+                @foreach( $guildEncounters as $encounter )
+                    @if ( array_key_exists($encounter->encounter_id, $encounterIDs))
+                        <tr>
+                            <td class="cellMobile" style="white-space:nowrap;">{{ array_key_exists($encounterIDs[$encounter->encounter_id]["name"],TauriBay\Encounter::ENCOUNTER_NAME_SHORTS) ? TauriBay\Encounter::ENCOUNTER_NAME_SHORTS[$encounterIDs[$encounter->encounter_id]["name"]] : $encounterIDs[$encounter->encounter_id]["name"] }}</td>
+                            <td class="cellDesktop" style="white-space:nowrap;">{{ $encounterIDs[$encounter->encounter_id]["name"] }}</td>
+                            <td class="cellDesktop">{{ TauriBay\Encounter::SIZE_AND_DIFFICULTY[$encounter->difficulty_id] }}</td>
+                            <td class="cellMobile">{{ TauriBay\Encounter::SIZE_AND_DIFFICULTY_SHORT[$encounter->difficulty_id] }}</td>
+                            <td class="cellDesktop">{{ date('M d, Y', $encounter->killtime) }}</td>
+                            <td class="cellMobile">{{ date('M d', $encounter->killtime) }}</td>
+                            <td><a class="guildClearTime" target="_blank" href="{{ URL::to("/encounter/") . "/" . TauriBay\Encounter::getUrlName($encounter->encounter_id) . "/" . $encounter->id }}">{{ $encounter->fight_time/1000  }}</a></td>
+                        </tr>
+                    @endif
+                @endforeach
+            </table>
+        </div>
+        <div class="text-center">
+            <div>
+                {{ $guildEncounters->appends(Illuminate\Support\Facades\Input::except('page')) }}
             </div>
         </div>
     </div>
