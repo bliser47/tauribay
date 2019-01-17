134bc0905d19306039d687bfac3ae8bd9014306f
root <simontamas47@gmail.com>
2019-01-17 14:35:24 +0000

    Revert "Fix massive typo"
    This reverts commit 662f5e83ba6e73a22bf4884d2f4ffdf37ead6cd3.

--- app/Encounter.php
+++ app/Encounter.php
@@ -259,13 +259,13 @@ class Encounter extends Model
             $member->class = $memberData["race"];
             $member->spec = $memberData["spec"];
             $member->damage_done = $memberData["dmg_done"];
-            $member->dps = $memberData["dmg_done"] / ( $encounter->fight_time / 1000 );
+            $member->dps = $memberData["dmg_done"] / ( $memberData["fight_time"] / 1000 );
             $member->damage_taken = $memberData["dmg_taken"];
             $member->damage_absorb = $memberData["dmg_absorb"];
             $member->heal_done = $memberData["heal_done"];
             $member->absorb_done = $memberData["absorb_done"];
             $member->overheal = $memberData["overheal"];
-            $member->hps = ($memberData["heal_done"] + $memberData["absorb_done"]) / ( $encounter->fight_time / 1000 );
+            $member->hps = ($memberData["heal_done"] + $memberData["absorb_done"]) / ( $memberData["fight_time"] / 1000 );
             $member->heal_taken = $memberData["heal_taken"];
             $member->interrupts = $memberData["interrupts"];
             $member->dispells = $memberData["dispells"];
