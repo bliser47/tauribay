beb8f69d2bf8a0180c167a1b0cbd533748446913
bliser47 <simontamas@live.com>
2019-01-30 13:21:16 +0000

    Fix speed kill numbering

--- resources/views/ladder/pve/ajax/speed.blade.php
+++ resources/views/ladder/pve/ajax/speed.blade.php
@@ -5,14 +5,13 @@
         <th>{{ __("Dátum") }}</th>
         <th>{{ __("Idő") }}</th>
     </tr>
-    @php $nr = 1 @endphp
-    @foreach( $encounters as $encounter )
+    @foreach( $encounters as $index=> $encounter )
         <tr>
             <td>
-                @if ( $nr < 4 )
-                    <img alt="" src="{{  URL::asset("img/award_small/" . $nr . ".png?v=4") }}"/>
+                @if ( ((($encounters->currentPage()-1)*10)+$index)  < 4 )
+                    <img alt="" src="{{  URL::asset("img/award_small/" . ((($encounters->currentPage()-1)*10)+$index)-1  . ".png?v=4") }}"/>
                 @else
-                    <b>{{ $nr }}</b>
+                    <b>{{ (($encounters->currentPage()-1)*10)+$index+1  }}</b>
                 @endif
             </td>
             <td class="cellDesktop faction-{{ $encounter["faction"] }}">
@@ -33,7 +32,6 @@
             <td class="cellMobile">{{ date('M d', $encounter->killtime) }}</td>
             <td><a class="guildClearTime" target="_blank" href="{{ URL::to("/encounter/") . "/" . TauriBay\Encounter::getUrlName($encounter->encounter_id) . "/" . $encounter->id }}">{{ $encounter->fight_time/1000  }}</a></td>
         </tr>
-        @php ++$nr @endphp
     @endforeach
 </table>
 <div class="text-center paginator">
