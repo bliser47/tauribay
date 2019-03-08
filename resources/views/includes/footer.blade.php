825357ca4ae13b13d3dbe97df017da6f376f698f
bliser47 <simontamas@live.com>
2019-03-08 12:10:19 +0000

    Add a necessary evil

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -97,3 +97,10 @@
 
     gtag('config', 'UA-30988835-1');
 </script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-6255828756943386",
+        enable_page_level_ads: true
+    });
+</script>
