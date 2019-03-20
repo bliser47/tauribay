d04741edfb83d29a09c8e449e71de659ba752ecd
bliser47 <simontamas@live.com>
2019-03-20 11:09:20 +0000

    Use app url in oauth redirect

--- resources/views/home/oauth.blade.php
+++ resources/views/home/oauth.blade.php
@@ -1,3 +1,3 @@
 <div class="panel-body login">
-    <a href="https://oauth.tauriwow.com/oauth/v2/auth?client_id=5_2i4bajinnracokc00480cogowgwkgwowk8os8k8ogogcw88g80&redirect_uri=https://tauribay.hu/oauth&response_type=code&scope=publicuserdata">Authorize</a>
+    <a href="https://oauth.tauriwow.com/oauth/v2/auth?client_id=5_2i4bajinnracokc00480cogowgwkgwowk8os8k8ogogcw88g80&redirect_uri={{ env('APP_URL') }}/oauth&response_type=code&scope=publicuserdata">Authorize</a>
 </div>
