93706031d0ebd5de551ece92eb36c0a4b145852d
Tamas Simon <thomas.simon@pushgaming.com>
2019-03-24 15:52:42 +0000

    Revert

--- app/Http/Controllers/EncounterController.php
+++ app/Http/Controllers/EncounterController.php
@@ -58,7 +58,8 @@ class EncounterController extends Controller
             ->first();
 
         if ( $encounter ) {
-            $members = EncounterMember::where("encounter_id", "=", $encounter->id)->leftJoin("character_encounters","character_encounters.encounter_member_id","=","encounter_members.id")->get();
+            //$members = EncounterMember::where("encounter_id", "=", $encounter->id)->leftJoin("character_encounters","character_encounters.encounter_member_id","=","encounter_members.id")->get();
+            $members = EncounterMember::where("encounter_id", "=", $encounter->id)->get();
 
             foreach ($members as $member) {
                 $member->total_heal = $member->heal_done + $member->absorb_done;
