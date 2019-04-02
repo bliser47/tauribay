3e6f39b72cd638cb48e6b746d75b4cf8082803d1
bliser47 <simontamas@live.com>
2019-04-02 19:12:42 +0100

    Misc

--- app/Http/Controllers/PveLadderController.php
+++ app/Http/Controllers/PveLadderController.php
@@ -527,7 +527,7 @@ class PveLadderController extends Controller
             $mapId = $_request->get("map_id", $_map_id);
             if ( $_request->has("difficulty_id"))
             {
-                $cacheKey = http_build_query($_request->all()) . "?v=14";
+                $cacheKey = http_build_query($_request->all()) . "?v=15";
                 $cacheValue = Cache::get($cacheKey);
                 $cacheUrlValue = Cache::get($cacheKey."URL");
                 if (  !$cacheValue ) {
