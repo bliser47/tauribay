84c525bc4ebf6bae23861b07c006c919c53e0d73
bliser47 <simontamas@live.com>
2019-04-12 12:40:38 +0100

    Set to empty array

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -27,6 +27,7 @@ class PlayerController extends Controller
         $cacheValue = Cache::get($cacheKey);
         if ( !$cacheValue || $calculate ) {
             $specBest = MemberTop::where("guid", "=", $guid)->where("encounter_id", "=", $encounterId)->where("difficulty_id", $difficultyId)->where("spec", "=", $specId)->first();
+            $cacheValue = array();
             if ($specBest && $calculate) {
                 $topType = EncounterMember::isHealer($specId) ? "hps" : "dps";
                 $encounter = $topType . "_encounter_id";
