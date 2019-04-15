8d2aaa9ea9da21145b3f48d3344bc616df6ec48c
bliser47 <simontamas@live.com>
2019-04-15 17:03:22 +0100

    gdkp application 50%

--- app/Http/Controllers/BliserGdkpController.php
+++ app/Http/Controllers/BliserGdkpController.php
@@ -3,13 +3,67 @@
 namespace TauriBay\Http\Controllers;
 
 use Illuminate\Http\Request;
+use TauriBay\AuthorizedCharacter;
+use TauriBay\Characters;
+use TauriBay\Defaults;
+use TauriBay\Encounter;
+use TauriBay\EncounterMember;
 use TauriBay\Http\Requests;
+use TauriBay\MemberTop;
+use TauriBay\Realm;
 use TauriBay\Tauri;
+use Auth;
+use TauriBay\Gdkp;
+
 
 class BliserGdkpController extends Controller
 {
+    public function apply(Request $_request)
+    {
+        $user = Auth::user();
+        if ( $user && $_request->has("character_id") ) {
+            $userHasChar = AuthorizedCharacter::where("user_id","=",$user->id)->where("character_id","=",$_request->get("character_id"))->first();
+            if ( $userHasChar ) {
+                $applied = Gdkp::where("character_id","=",$_request->get("character_id"))->first();
+                if ( !$applied ) {
+                    $character = Characters::where("character_id",$_request->get("character_id"))->first();
+                    $apply = new Gdkp;
+                    $apply->account_id = $user->id;
+                    $apply->character_id = $_request->get("character_id");
+                    $totalScore = 0;
+                    $ids = Encounter::getMapEncountersIds(Defaults::EXPANSION_ID, Defaults::MAP_ID);
+                    foreach ( $ids as $id ) {
+                        $topScore = MemberTop::where("realm_id","=",$character->realm)->where("name","=",$character->name)
+                            ->where("encounter_id","=",$id)->whereIn("difficulty_id",array(4,6))->orderBy("")
+                    }
+                    $score = $totalScore;
+                    $apply->score = 0;
+                    $apply->save();
+                }
+            }
+        }
+        return $this->index($_request);
+    }
+
     public function index(Request $_request)
     {
-        return view("gdkp");
+        $user = Auth::user();
+        if ($user) {
+            $applied = Gdkp::leftJoin("characters", "characters.id", "=", "gdkps.character_id")->orderBy("score", "desc")->get();
+            $appliedIds = array();
+            foreach ( $applied as $char ) {
+                $appliedIds[] = $char->character_id;
+            }
+            $charactersResult = AuthorizedCharacter::where("user_id", "=", $user->id)
+                ->where("realm", "=", Realm::TAURI)->whereNotIn("character_id",$appliedIds)
+                ->leftJoin("characters", "characters.id", "=", "authorized_characters.character_id")->get();
+            $characters = array();
+            foreach ($charactersResult as $char) {
+                $characters[$char->id] = "[" . Realm::REALMS_SHORT[$char->realm] . "] " . $char->name;
+            }
+            $characterClasses = Tauri\CharacterClasses::CHARACTER_CLASS_NAMES;
+            return view("gdkp", compact("characters", "applied", "characterClasses"));
+        }
+        return redirect()->route('login');
     }
 }
